a9b0cfb0ad495a9ea402028aba056f709a1fc3fd
Artem <rumarchyk204@gmail.com>
2025-04-22 10:07:02 +0200

    added icons and expo-splash-screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,59 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import CustomModal from "@/components/CustomModal";
 import { PaperProvider } from 'react-native-paper';
 import { ThemeProvider, ThemeContext } from '@/contexts/ThemeContext';
 import { ModalProvider } from '@/contexts/ModalContext';
 import { DatabaseProvider } from '@/contexts/DatabaseContext';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as SplashScreen from 'expo-splash-screen';
 // Status bar component to change the color of the status bar based on the theme since default 'auto' option doesn't work
 function AppStatusBar() {
   const { theme } = useContext(ThemeContext);
   return <StatusBar style={theme === "dark" ? "light" : "dark"} />;
 }
 
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 400,
+  fade: true,
+})
+
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <PaperProvider>
         <ThemeProvider>
           <DatabaseProvider>

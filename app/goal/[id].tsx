cc276cd0bee2d0bbd4bed9dc6eea0f6ee4966466
Artem <rumarchyk204@gmail.com>
2025-05-09 09:40:57 +0200

    no comments

--- app/goal/[id].tsx
+++ app/goal/[id].tsx
@@ -426,7 +426,7 @@ export default function Goal() {
                                             displayCheckbox={false}
                                             displayBorders={true}
                                             onPress={() => router.push({
-                                                pathname: '/task',
+                                                pathname: '/habit',
                                                 params: {
                                                     goalId: id,
                                                     habitId: habit.id,
@@ -445,7 +445,7 @@ export default function Goal() {
                                 <TouchableOpacity
                                     style={[styles.addButton, styles.addHabitButtonBgColor]}
                                     onPress={() => router.push({
-                                        pathname: '/task',
+                                        pathname: '/habit',
                                         params: { goalId: id }
                                     })}
                                 >
